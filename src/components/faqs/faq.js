0334e65184295bd3619ebdb726241f3d33be26fd
Guillermo <guillef33@gmail.com>
2022-01-10 18:37:33 -0300

    textos faqs y reviews

--- src/components/faqs/faq.js
+++ src/components/faqs/faq.js
@@ -1,28 +1,28 @@
 const questions = [
   {
     id: 1,
-    title: "Do I have to allow the use of cookies?",
-    info: "Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.",
+   title: "Que modalidad de trabajo hago?",
+   info: "Trabajo por proyectos y freelance.",
   },
   {
     id: 2,
-    title: "How do I change my My Page password?",
-    info: "Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.",
+    title: "Que lenguajes manejas?",
+    info: "Tengo gran experiencia en desarrollos en WordPress. Actualmente estoy desarrollando en React. ",
   },
   {
     id: 3,
-    title: "What is BankID?",
-    info: "Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.",
+    title: "Sos mas developer o mas PM?",
+    info: "Como PM me considero senior. Como dev me considero jr. ",
   },
   {
     id: 4,
-    title: "Whose birth number can I use?",
-    info: "Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.",
+    title: "Has trabajado para empresas en el exterior?",
+    info: "Si, principalmente para Estados Unidos y Espana",
   },
   {
     id: 5,
-    title: "When do I recieve a password ordered by letter?",
-    info: "Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ",
+    title: "Que nivel de ingles tienes?",
+    info: "Buen nivel de ingles, escrito y oral.",
   },
 ];
 export default questions;
--- src/components/reviews/data.js
+++ src/components/reviews/data.js
@@ -1,35 +1,35 @@
 const reviews = [
   {
     id: 1,
-    name: "susan smith",
-    job: "web developer",
+    name: "Justiniano Vila",
+    job: "Uanaknow",
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
-    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    text: "Trabajo con Guillermo hace mas de 2 anos. Siempre buena predisposicion y conocimiento.",
   },
   {
     id: 2,
-    name: "anna johnson",
-    job: "web designer",
+    name: "Emanuel Roca",
+    job: "Conaxis",
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
-    text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+    text: "Trabajo con Guillermo hace mas de 2 anos. Siempre buena predisposicion y conocimiento.",
   },
   {
     id: 3,
-    name: "peter jones",
-    job: "intern",
+    name: "Alfredo Palmieri",
+    job: "CloudWorks",
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
-    text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+    text: "Trabajo con Guillermo hace mas de 2 anos. Siempre buena predisposicion y conocimiento.",
   },
   {
     id: 4,
-    name: "bill anderson",
-    job: "the boss",
+    name: "Michel Fauve",
+    job: "The Papaya Group",
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
-    text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+    text: "Trabajo con Guillermo hace mas de 2 anos. Siempre buena predisposicion y conocimiento.",
   },
 ];
 
